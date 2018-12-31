6deaf37888e7fa4e432296a58a3e2ce324b32c72
leonidleonid <36963539+leonidleonid@users.noreply.github.com>
2018-12-31 07:44:02 +0300

    added previews section +

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 
 var parallaxBox = document.querySelector('.reviews-bg');
-var yPos = window.pageYOffset;
 
 window.onscroll = function () {
     var yPos = window.pageYOffset;
-    console.log(yPos);
     if (yPos >= 660) {
         parallaxBox.style.transform = 'translateY(' + (yPos-1100)/3 + 'px)';
-        console.log(parallaxBox.style.transform);
     }
 };
